eb2d4b20c4eb1ecd97893d28dcec3ef08485ceb1
sherryl0709 <b05701101@ntu.edu.tw>
2020-06-03 22:50:31 +0800

    與task2連接完畢
    更新csv
    15題後要結束這個問題還沒解決

--- src/task4.py
+++ src/task4.py
@@ -1,80 +1,110 @@
 import csv
 
 # 讀取問題存進dict
-file = "src//question_part1.csv"
+file = "src\\question_part1.csv"
 filesopen = open(file, 'r', newline='')
 rows = csv.reader(filesopen)
-question_dict = dict()
+question_list = []
 for row in rows:
-    question_dict[int(row[0])] = row[1:]
+    question_list.append(row)
 
 # 讀取貓咪資料存進dict
-file1 = "src//貓咪.csv"
+file1 = "src\\貓咪.csv"
 filesopen = open(file1, 'r', newline='')
 rows = csv.reader(filesopen)
 cat_dict = dict()
 for row in rows:
     cat_dict[int(row[0])] = row[1:]
 
-# print(question_dict)
-# print(cat_dict)
-    
+q_id = 0
 
-def get_question(question_dict, q_id, color_list):
-    '''取出問題及答案文字'''
-    if q_id <= 12:
-        q_content = question_dict[q_id][0]
-        ans_1 = question_dict[q_id][1]
-        ans_2 = question_dict[q_id][2]
-    else:
-        if q_id == 13:
-            q_content = question_dict[q_id][0]+color_list[0]+"還是"+color_list[1]+"？"
-            ans_1 = color_list[0]
-            ans_2 = color_list[1]
-        elif q_id == 14:
-            q_content = question_dict[q_id][0]+color_list[1]+"還是"+color_list[2]+"？"
-            ans_1 = color_list[1]
-            ans_2 = color_list[2]
-        else:
-            q_content = question_dict[q_id][0]+color_list[0]+"還是"+color_list[2]+"？"
-            ans_1 = color_list[0]
-            ans_2 = color_list[2]
-    return q_content, ans_1, ans_2
-
-def click_ans1(question_dict, q_id):
-    '''按第一個鈕'''
-    ans = question_dict[q_id][6]
-    adjust_type = question_dict[q_id][5]
-    next_q_id = question_dict[q_id][3]
-    
-    return ans, adjust_type, next_q_id
-
-def click_ans2(question_dict, q_id):
-    '''按第二個鈕'''
-    ans = question_dict[q_id][7]
-    adjust_type = question_dict[q_id][5]
-    next_q_id = question_dict[q_id][4]
-    return ans, adjust_type, next_q_id
-
-
-# 創建貓咪分數字典
-cat_score = {}
-for cat_id in cat_dict.keys():
-    cat_score[int(cat_id)] = 60 # 分數60起跳    
-    
-def calculate_score(question_dict, cat_dict, cat_score, q_id, ans, adjust_type):
-    '''計算分數加減並更新'''
-    if q_id <= 9:
-        for cat_id in cat_score.keys():
-            if ans == cat_dict[cat_id][int(adjust_type)]:
-                cat_score[cat_id] += int(question_dict[q_id][8])
+def click1(question_list, cat_dict, cat_score, color_list, i):
+    global q_id
+    global stop
+    for cat_id in cat_dict.keys():  # 計算分數
+        if q_id <= 12:
+            if question_list[q_id][1] == cat_dict[cat_id][q_id]:
+                cat_score[cat_id] += int(question_list[q_id][3])
             else:
-                cat_score[cat_id] += int(question_dict[q_id][9])
+                cat_score[cat_id] += int(question_list[q_id][4])
+        else:
+            if q_id == 13:
+                if color_list[0] == cat_dict[cat_id][q_id]:
+                    cat_score[cat_id] += int(question_list[q_id][3])
+            elif q_id == 14:
+                if color_list[1] == cat_dict[cat_id][q_id]:
+                    cat_score[cat_id] += int(question_list[q_id][3])
+            else:
+                if color_list[0] == cat_dict[cat_id][q_id]:
+                    cat_score[cat_id] += int(question_list[q_id][3])
+    if q_id >= 10:  # 製造最後三題
+        if q_id <= 12:
+            color_list.append(question_list[q_id][1])
+    if q_id < 15:  # 進下一題
+        q_id += 1
+    if q_id <= 15:
+        if q_id == 13:
+            questionlbl.config(text = question_list[q_id][0]+color_list[0]+"還是"+color_list[1]+"？")
+            ans_btn1.config(text = color_list[0])
+            ans_btn2.config(text = color_list[1])
+        elif q_id == 14:
+            questionlbl.config(text = question_list[q_id][0]+color_list[1]+"還是"+color_list[2]+"？")
+            ans_btn1.config(text = color_list[1])
+            ans_btn2.config(text = color_list[2])
+        elif q_id == 15:
+            questionlbl.config(text = question_list[q_id][0]+color_list[0]+"還是"+color_list[2]+"？")
+            ans_btn1.config(text = color_list[0])
+            ans_btn2.config(text = color_list[2])
+        else:
+            questionlbl.config(text = question_list[q_id][0])
+            ans_btn1.config(text = question_list[q_id][1])
+            ans_btn2.config(text = question_list[q_id][2])
     else:
-        for cat_id in cat_score.keys():
-            if ans == cat_dict[cat_id][int(adjust_type)]:
-                cat_score[cat_id] += 2
-    return cat_score
+        pass
+
+def click2(question_list, cat_dict, cat_score, color_list, i):
+    global q_id
+    global stop
+    for cat_id in cat_dict.keys():  # 計算分數
+        if q_id <= 12:
+            if question_list[q_id][2] == cat_dict[cat_id][q_id]:
+                cat_score[cat_id] += int(question_list[q_id][5])
+            else:
+                cat_score[cat_id] += int(question_list[q_id][6])
+        else:
+            if q_id == 13:
+                if color_list[1] == cat_dict[cat_id][q_id]:
+                    cat_score[cat_id] += int(question_list[q_id][3])
+            elif q_id == 14:
+                if color_list[2] == cat_dict[cat_id][q_id]:
+                    cat_score[cat_id] += int(question_list[q_id][3])
+            else:
+                if color_list[2] == cat_dict[cat_id][q_id]:
+                    cat_score[cat_id] += int(question_list[q_id][3])
+    if q_id >= 10:  # 製造最後三題
+        if q_id <= 12:
+            color_list.append(question_list[q_id][2])
+    if q_id < 15:  # 進下一題
+        q_id += 1
+    if q_id <= 15:
+        if q_id == 13:
+            questionlbl.config(text = question_list[q_id][0]+color_list[0]+"還是"+color_list[1]+"？")
+            ans_btn1.config(text = color_list[0])
+            ans_btn2.config(text = color_list[1])
+        elif q_id == 14:
+            questionlbl.config(text = question_list[q_id][0]+color_list[1]+"還是"+color_list[2]+"？")
+            ans_btn1.config(text = color_list[1])
+            ans_btn2.config(text = color_list[2])
+        elif q_id == 15:
+            questionlbl.config(text = question_list[q_id][0]+color_list[0]+"還是"+color_list[2]+"？")
+            ans_btn1.config(text = color_list[0])
+            ans_btn2.config(text = color_list[2])
+        else:
+            questionlbl.config(text = question_list[q_id][0])
+            ans_btn1.config(text = question_list[q_id][1])
+            ans_btn2.config(text = question_list[q_id][2])
+    else:
+        pass
 
 def get_3_cat(cat_score):
     '''找分數前三高貓咪'''
@@ -82,7 +112,7 @@ def get_3_cat(cat_score):
     for i in range(3):
         max = -1
         max_id = []
-        for cat_id in cat_score:
+        for cat_id in cat_score.keys():
             if cat_score[cat_id] > max:
                 max_id = []
                 max = cat_score[cat_id]
@@ -93,24 +123,38 @@ def get_3_cat(cat_score):
                 pass
         for id in max_id:
             max_id_all.append(id)
+            del cat_score[id]
         if len(max_id_all)>3:
             break
     return(max_id_all)
-    
-q_id = 1
-next_q_id = 1
+
+import tkinter as tk
+import random
+
+
+win = tk.Tk()
+win.geometry('500x300')
+win.config(bg = '#323232')
+
+
 color_list = []
-while q_id != 0:
-    q_content, ans_1, ans_2 = get_question(question_dict, q_id, color_list)
-    ans, adjust_type, next_q_id = click_ans2(question_dict, q_id)
-    cat_score = calculate_score(question_dict, cat_dict, cat_score, q_id, ans, adjust_type)
-    q_id = int(next_q_id)
-    print(q_content)
-    if q_id >= 11:
-        if q_id <= 13:
-            color_list.append(ans_1)
+cat_score = {}
+for cat_id in cat_dict.keys():
+    cat_score[int(cat_id)] = 60 # 分數60起跳    
+
+questionlbl = tk.Label(text = question_list[0][0], bg = 'gray', fg = 'white', font = '微軟正黑體 25')
+questionlbl.place(anchor = 'center', relx = 0.5, rely = 0.45)
+ans_btn1 = tk.Button(text = question_list[0][1], height = 1, width = 5, font = '微軟正黑體 20', command = lambda: click1(question_list, cat_dict, cat_score, color_list, q_id))
+ans_btn2 = tk.Button(text = question_list[0][2], height = 1, width = 5, font = '微軟正黑體 20', command = lambda: click2(question_list, cat_dict, cat_score, color_list, q_id))
+ans_btn1.place(anchor = 'center', relx = 0.3, rely = 0.8)
+ans_btn2.place(anchor = 'center', relx = 0.7, rely = 0.8) 
+win.mainloop()
 
 print(cat_score)
-
 max_id_all = get_3_cat(cat_score)
 print(max_id_all)
+max_id_all = random.sample(max_id_all,3)
+print(max_id_all)
+
+
+
