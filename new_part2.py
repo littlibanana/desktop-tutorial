c41eba4bf8217bd5feba935323c62356c010d2b7
littlibanana <66011133+littlibanana@users.noreply.github.com>
2020-06-05 02:19:39 +0800

    增加評分機制，改善函數

--- new_part2.py
+++ new_part2.py
@@ -20,14 +20,8 @@ def read_PART2():
 
             question_list.append(question_dict)
 
-    # random_num = random.choices(range(0, 29), k=10)
-    random_num = []
-    while len(random_num) <= 10:
-        tmp_num = random.choice(range(0, 29))
-        if tmp_num not in random_num:
-            random_num.append(tmp_num)
+    random_num = random.choices(range(0, 29), k=10)
     print(random_num)
-
     final_question = []
     for num in random_num:
         final_question.append(question_list[num])
@@ -35,15 +29,38 @@ def read_PART2():
     return final_question
 
 
+def count_grade():
+    results = []
+    for i in range(0, 10):
+        results.append(answer[i])
+    # print(results)
+    score = 100
+    for i in range(0, 10):
+        if str(results[i]) == '1':
+            score += int(btn_1_score[i])
+        if str(results[i]) == '2':
+            score += int(btn_2_score[i])
+    return score
+
+
 questions = []
 option1s = []
 option2s = []
+true_answers = []
+info = []
+btn_1_score = []
+btn_2_score = []
+
 questions_list = read_PART2()
 
 for q in questions_list:
     questions.append(q['question'])
     option1s.append(q['option1'])
     option2s.append(q['option2'])
+    true_answers.append(q['answer'])
+    info.append(q['info'])
+    btn_1_score.append(q['score1'])
+    btn_2_score.append(q['score2'])
 
 
 answer = {}
@@ -63,9 +80,9 @@ class Part2(tk.Frame):
         self.questionlbl.place(anchor='center', relx=0.5, rely=0.45)
 
         self.ans_btn1 = tk.Button(
-            text=option1s[number], height=1, font='微軟正黑體 20', command=lambda: self.btn1(number))
+            text=option1s[number], height=1, width=20, font='微軟正黑體 20', command=lambda: self.btn1(number))
         self.ans_btn2 = tk.Button(
-            text=option2s[number], height=1, font='微軟正黑體 20', command=lambda: self.btn2(number))
+            text=option2s[number], height=1, width=20, font='微軟正黑體 20', command=lambda: self.btn2(number))
         self.ans_btn1.place(anchor='center', relx=0.3, rely=0.8)
         self.ans_btn2.place(anchor='center', relx=0.7, rely=0.8)
 
@@ -75,38 +92,53 @@ class Part2(tk.Frame):
 
     def btn1(self, i):
         global number
-        if number < 9:
+        if number <= 9:
             number += 1
-        temp = questions[i]
-        answer[temp] = 1
-        i += 1
-        if i <= 9:
-            self.questionlbl.config(text=questions[i])
-            self.ans_btn1.config(text=option1s[i])
-            self.ans_btn2.config(text=option2s[i])
-        print(answer)
+
+        answer[number-1] = 1
+
+        if number <= 9:
+            self.questionlbl.config(text=str(number+1)+'.'+questions[number])
+            self.ans_btn1.config(text=option1s[number])
+            self.ans_btn2.config(text=option2s[number])
+
+        if number == 10:
+            part2_grade = count_grade()
+            self.questionlbl.config(text=part2_grade)
+            self.ans_btn1.destroy()
+            self.ans_btn2.destroy()
+            self.back_btn.config(text='回到首頁')
+        print(answer, number)
 
     def btn2(self, i):
         global number
-        if number < 9:
+        if number <= 9:
             number += 1
-        answer[questions[i]] = 2
-        i += 1
-        if i <= 9:
-            self.questionlbl.config(text=questions[i])
-            self.ans_btn1.config(text=option1s[i])
-            self.ans_btn2.config(text=option2s[i])
-        print(answer)
+
+        answer[number-1] = 2
+
+        if number <= 9:
+            self.questionlbl.config(text=str(number+1)+'.'+questions[number])
+            self.ans_btn1.config(text=option1s[number])
+            self.ans_btn2.config(text=option2s[number])
+
+        if number == 10:
+            part2_grade = count_grade()
+            self.questionlbl.config(text=part2_grade)
+            self.ans_btn1.destroy()
+            self.ans_btn2.destroy()
+            self.back_btn.config(text='回到首頁')
+        print(answer, number)
 
     def back(self):
         global number
         if number > 0:
             number -= 1
-        answer[questions[number]] = ''
-        self.questionlbl.config(text=questions[number])
+        answer[number] = ''
+        self.questionlbl.config(text=str(number+1)+','+questions[number])
         self.ans_btn1.config(text=option1s[number])
         self.ans_btn2.config(text=option2s[number])
-        print(answer)
+        print(answer, number)
 
 
 part2 = Part2()
