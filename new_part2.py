fc825c1706663d089255440b0ac9b609d4e0b7df
littlibanana <66011133+littlibanana@users.noreply.github.com>
2020-06-06 16:05:55 +0800

    增加答對與答錯之介面

--- new_part2.py
+++ new_part2.py
@@ -10,7 +10,7 @@ track = pygame.mixer.music.load(file)
 pygame.mixer.music.play()
 
 
-def read_PART2():
+def read_PART2_question():
 
     with open(file=r'C:/Users\User\Desktop\part2.csv', mode='r', encoding='utf-8-sig') as part2:
         question_list = []  # list of dict
@@ -41,29 +41,17 @@ def read_PART2():
     return final_question
 
 
-def count_grade():
-    results = []
-    for i in range(0, 10):
-        results.append(answer[i])
-    # print(results)
-    score = 100
-    for i in range(0, 10):
-        if str(results[i]) == '1':
-            score += int(btn_1_score[i])
-        if str(results[i]) == '2':
-            score += int(btn_2_score[i])
-    return score
-
-
 questions = []
 option1s = []
 option2s = []
 true_answers = []
+
 info = []
 btn_1_score = []
 btn_2_score = []
 
-questions_list = read_PART2()
+questions_list = read_PART2_question()
+
 
 for q in questions_list:
     questions.append(q['question'])
@@ -74,7 +62,7 @@ for q in questions_list:
     btn_1_score.append(q['score1'])
     btn_2_score.append(q['score2'])
 
-
+print(true_answers)
 answer = {}
 number = 0
 
@@ -102,46 +90,6 @@ class Part2(tk.Frame):
             text='上一題', font='微軟正黑體 15', command=self.back)
         self.back_btn.place(anchor='center', relx=0.9, rely=0.9)
 
-    def btn1(self, i):
-        global number
-        if number <= 9:
-            number += 1
-
-        answer[number-1] = 1
-
-        if number <= 9:
-            self.questionlbl.config(text=str(number+1)+'.'+questions[number])
-            self.ans_btn1.config(text=option1s[number])
-            self.ans_btn2.config(text=option2s[number])
-
-        if number == 10:
-            part2_grade = count_grade()
-            self.questionlbl.config(text=part2_grade)
-            self.ans_btn1.destroy()
-            self.ans_btn2.destroy()
-            self.back_btn.config(text='回到首頁')
-        print(answer, number)
-
-    def btn2(self, i):
-        global number
-        if number <= 9:
-            number += 1
-
-        answer[number-1] = 2
-
-        if number <= 9:
-            self.questionlbl.config(text=str(number+1)+'.'+questions[number])
-            self.ans_btn1.config(text=option1s[number])
-            self.ans_btn2.config(text=option2s[number])
-
-        if number == 10:
-            part2_grade = count_grade()
-            self.questionlbl.config(text=part2_grade)
-            self.ans_btn1.destroy()
-            self.ans_btn2.destroy()
-            self.back_btn.config(text='回到首頁')
-        print(answer, number)
-
     def back(self):
         global number
         if number > 0:
@@ -151,6 +99,94 @@ class Part2(tk.Frame):
         self.ans_btn1.config(text=option1s[number])
         self.ans_btn2.config(text=option2s[number])
         print(answer, number)
+
+    def part2_count_grade(self):
+        results = []
+        for i in range(0, 10):
+            results.append(answer[i])
+        # print(results)
+        score = 100
+        for i in range(0, 10):
+            if str(results[i]) == '1':
+                score += int(btn_1_score[i])
+            if str(results[i]) == '2':
+                score += int(btn_2_score[i])
+        return score
+
+    def btn1(self, i):
+        global number
+        if number <= 9:
+            number += 1
+
+        answer[number-1] = 1
+
+        if answer[number-1] == int(true_answers[number-1]):
+            self.questionlbl.config(text='恭喜答對')
+            if number <= 9:
+                self.back_btn.config(
+                    text='下一題', command=lambda: self.next(number))
+            if number == 10:
+                self.back_btn.config(
+                    text='點我看分數', command=lambda: self.next(number))
+            self.ans_btn1.place_forget()
+            self.ans_btn2.place_forget()
+
+        if answer[number-1] != int(true_answers[number-1]):
+            self.questionlbl.config(text='答錯囉，因為'+info[number-1])
+            if number <= 9:
+                self.back_btn.config(
+                    text='下一題', command=lambda: self.next(number))
+            if number == 10:
+                self.back_btn.config(
+                    text='點我看分數', command=lambda: self.next(number))
+            self.ans_btn1.place_forget()
+            self.ans_btn2.place_forget()
+
+    def btn2(self, i):
+        global number
+        if number <= 9:
+            number += 1
+
+        answer[number-1] = 2
+
+        if answer[number-1] == int(true_answers[number-1]):
+            self.questionlbl.config(text='恭喜答對ฅ’ω’ฅ')
+            if number <= 9:
+                self.back_btn.config(
+                    text='下一題', command=lambda: self.next(number))
+            if number == 10:
+                self.back_btn.config(
+                    text='點我看分數', command=lambda: self.next(number))
+            self.ans_btn1.place_forget()
+            self.ans_btn2.place_forget()
+
+        if answer[number-1] != int(true_answers[number-1]):
+            self.questionlbl.config(text='答錯囉!'+info[number-1])
+            if number <= 9:
+                self.back_btn.config(
+                    text='下一題', command=lambda: self.next(number))
+            if number == 10:
+                self.back_btn.config(
+                    text='點我看分數', command=lambda: self.next(number))
+            self.ans_btn1.place_forget()
+            self.ans_btn2.place_forget()
+
+        print(answer, number)
+
+    def next(self, i):
+        if number <= 9:
+            self.questionlbl.config(text=str(number+1)+'.'+questions[number])
+            self.ans_btn1.config(text=option1s[number])
+            self.ans_btn2.config(text=option2s[number])
+            self.ans_btn1.place(anchor='center', relx=0.3, rely=0.8)
+            self.ans_btn2.place(anchor='center', relx=0.7, rely=0.8)
+            self.back_btn.config(text='上一題', command=self.back)
+        if number == 10:
+            part2_grade = self.part2_count_grade()
+            self.questionlbl.config(text='你的分數是:'+str(part2_grade)+'分')
+            self.ans_btn1.place_forget()
+            self.ans_btn2.place_forget()
+            self.back_btn.config(text='回到首頁')  # 這裡加上回到主葉面的函數
 
 
 part2 = Part2()
